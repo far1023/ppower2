bc92f3b8709f3bc235adced25af3d4987bc8fe23
Fuad Agil <far1023@users.noreply.github.com>
2020-11-18 10:38:49 +0700

    uuid generator

---
+++ application/helpers/uuid_helper.php
@@ -0,0 +1,8 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+function uuid_gen() {
+	$data = random_bytes(16);
+	$data[6] = chr(ord($data[6]) & 0x0f | 0x40); 
+	$data[8] = chr(ord($data[8]) & 0x3f | 0x80); 
+	return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+}
